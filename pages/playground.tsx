4ec238bb48e2db4f6b5196ebb8a4f38ed68b9590
nithinps021 <nithinps021@gmail.com>
2022-10-19 19:35:40 +0530

    setup tailwindcss

---
+++ pages/playground.tsx
@@ -0,0 +1,11 @@
+import next, { NextPage } from "next";
+
+const Playground : NextPage = () =>{
+    return (
+        <div>
+            <h1>playground</h1>
+        </div>
+    )
+}
+
+export default Playground;
